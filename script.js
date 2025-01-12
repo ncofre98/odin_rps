ba0dfbfab77bf2b426da9ceebce019cb50751756
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-01-11 19:45:51 -0600

    Created playRound function and p1Wins as helper function to determine whether p1 has won the round

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const choices = ['rock', 'paper', 'scissors'];
-let humanScore, computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const random = Math.floor(Math.random() * choices.length);
@@ -13,5 +14,27 @@ function getHumanChoice() {
     return choices[choice - 1];
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+// 0 for rock, 1 for paper, 2 for scissors
+// returns true if player1 wins, otherwise it returns false
+function p1Wins(p1, p2) {
+    return (p1 == 'rock' && p2 == 'scissors' || p1 == 'paper' && p2 == 'rock' || p1 == 'scissors' && p2 == 'paper');
+}
+
+function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
+    if (humanChoice == computerChoice)
+        return `Draw! Both of you choosed ${humanChoice}`;
+    if (p1Wins(humanChoice, computerChoice)) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    }
+        else {
+            computerScore++;
+            return `You lose! ${computerChoice} beats ${humanChoice}`;
+        }
+}
+
+// console.log(getComputerChoice());
+// console.log(getHumanChoice());
+console.log(playRound());
+console.log("Human score: " + humanScore);
+console.log("Computer score: " + computerScore);
