a6a1e472b259f88ef1c94d675ec8f452cf39ae44
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-01-20 15:15:45 -0600

    Basic UI finished

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const CHOICES = ['rock', 'paper', 'scissors'];
 const NUMBER_OF_ROUNDS = 5;
 
+//UI Elements
+const buttons = document.querySelectorAll('.btn');
+const resultsTxt = document.getElementById('results');
+const playerScoreTxt = document.getElementById('playerScore');
+const computerScoreTxt = document.getElementById('computerScore');
+const nRoundsTxt = document.getElementById('nRounds');
+const instructionTxt = document.getElementById('instructionTxt');
+
 function getComputerChoice() {
     const random = Math.floor(Math.random() * CHOICES.length);
 
     return CHOICES[random];
 }
 
-function getHumanChoice() {
-    const choice = prompt("What's your choice? 1.Rock 2.Paper 3.Scissors");
+function getHumanChoice(playerSelection) {
+    let choice = playerSelection.target.textContent.toLowerCase();
+    choice = CHOICES.indexOf(choice);
 
-    return CHOICES[choice - 1];
+    return CHOICES[choice];
 }
 
 // 0 for rock, 1 for paper, 2 for scissors
@@ -19,31 +28,44 @@ function p1Wins(p1, p2) {
     return (p1 == 'rock' && p2 == 'scissors' || p1 == 'paper' && p2 == 'rock' || p1 == 'scissors' && p2 == 'paper');
 }
 
+function displayResult(txt) {
+    instructionTxt.textContent = txt;
+}
+
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
-    let finalResult = "";
+    let nRounds = 0;
 
-    function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
+    function playRound(playerSelection, humanChoice = getHumanChoice(playerSelection), computerChoice = getComputerChoice()) {
+        nRoundsTxt.textContent = ++nRounds;
         if (humanChoice == computerChoice)
             return `Draw! Both of you choosed ${humanChoice}`;
         if (p1Wins(humanChoice, computerChoice)) {
-            humanScore++;
+            playerScoreTxt.textContent = ++humanScore;
             return `You win! ${humanChoice} beats ${computerChoice}`;
         }
             else {
-                computerScore++;
+                computerScoreTxt.textContent = ++computerScore;
                 return `You lose! ${computerChoice} beats ${humanChoice}`;
             }
     }
-    for (let i = 0; i < NUMBER_OF_ROUNDS ; i++) {
-        console.log(playRound());
+
+    function getFinalResults(humanScore, computerScore) {
+        finalResult = (humanScore == computerScore) ? 'Draw! '
+        : (humanScore > computerScore) ? 'You won! ' : 'You lose! ';
+        finalResult += `Human Scored: ${humanScore}; Computer Scored: ${computerScore}`;
+
+        return finalResult;
     }
 
-    finalResult = (humanScore == computerScore) ? 'Draw! '
-        : (humanScore > computerScore) ? 'You won! ' : 'You lose! ';
-    finalResult += `Human Scored: ${humanScore}; Computer Scored: ${computerScore}`;
-    return finalResult;
+    buttons.forEach(button => button.addEventListener('click', (e) => {
+        displayResult(playRound(e));
+        if (nRounds === NUMBER_OF_ROUNDS) {
+            buttons.forEach(button => button.disabled = true);
+            displayResult(getFinalResults(humanScore, computerScore));
+        }
+    }));
 }
 
-console.log(playGame());
+playGame();
